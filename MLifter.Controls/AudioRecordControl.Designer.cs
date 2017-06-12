8dc80e4df1225946cc3ead0f5047d94ae42a082d
SoftCorp <SoftCorp@SOFTCORP-PC>
2017-06-12 13:38:03 +0700

    Add project files.

---
+++ MLifter.Controls/AudioRecordControl.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace MLifter.Controls
+{
+    partial class AudioRecordControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonRecord = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Image = global::MLifter.Controls.Properties.Resources.process_stop;
+            this.buttonRemove.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.buttonRemove.Location = new System.Drawing.Point(160, 0);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 28);
+            this.buttonRemove.TabIndex = 21;
+            this.buttonRemove.TabStop = false;
+            this.buttonRemove.Text = "&Delete";
+            this.buttonRemove.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonRemove.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonRecord
+            // 
+            this.buttonRecord.Image = global::MLifter.Controls.Properties.Resources.mediaRecord;
+            this.buttonRecord.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.buttonRecord.Location = new System.Drawing.Point(80, 0);
+            this.buttonRecord.Name = "buttonRecord";
+            this.buttonRecord.Size = new System.Drawing.Size(75, 28);
+            this.buttonRecord.TabIndex = 19;
+            this.buttonRecord.Text = "&Record";
+            this.buttonRecord.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonRecord.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonRecord.UseVisualStyleBackColor = true;
+            this.buttonRecord.Click += new System.EventHandler(this.buttonRecord_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Image = global::MLifter.Controls.Properties.Resources.mediaPlaybackStart;
+            this.buttonPlay.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.buttonPlay.Location = new System.Drawing.Point(0, 0);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 28);
+            this.buttonPlay.TabIndex = 20;
+            this.buttonPlay.Text = "&Play";
+            this.buttonPlay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonPlay.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // AudioRecordControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonRecord);
+            this.Controls.Add(this.buttonPlay);
+            this.Name = "AudioRecordControl";
+            this.Size = new System.Drawing.Size(238, 30);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonRecord;
+        private System.Windows.Forms.Button buttonPlay;
+    }
+}
---
+++ MLifter.Controls/AudioRecordControl.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using LameDOTnet;
+using MLifter.AudioTools;
+using MLifter.Controls.Properties;
+
+namespace MLifter.Controls
+{
+    public partial class AudioRecordControl : UserControl
+    {
+        public delegate void UserControlClickHandler(object sender, EventArgs e);
+        public event UserControlClickHandler buttonPlayClick;
+        public event UserControlClickHandler buttonRecordClick;
+        public event UserControlClickHandler buttonDeleteClick;
+        public AudioRecordControl()
+        {
+            InitializeComponent();
+            recorder = new Recorder(Settings.Default.audioChannels, Settings.Default.audioSamplingrate);
+        }
+        private AudioPlayer player;
+        private Recorder recorder;
+
+        private string path = string.Empty;
+        public string RecordPath
+        {
+            get { return path; }
+            set
+            {
+                path = value;
+                buttonPlay.Enabled = (path != string.Empty && SoundDevicesAvailable.SoundOutDeviceAvailable());
+            }
+        }
+
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (player == null)
+                {
+                    player = new AudioPlayer();
+                    player.OnEnd += PlayerOnOnEnd;
+                }
+
+                if (!player.IsPlaying)
+                {
+                    buttonRecord.Enabled = false;
+                    buttonRemove.Enabled = false;
+
+                    buttonPlay.Image = Resources.mediaPlaybackStop;
+                    buttonPlay.Text = Resources.AUDIO_STOP;
+
+                    player.Play(RecordPath);
+                }
+                else
+                {
+                    player.Stop();
+                    buttonRecord.Enabled = SoundDevicesAvailable.SoundInDeviceAvailable();
+                    buttonRemove.Enabled = true;
+
+                    buttonPlay.Image = Resources.mediaPlaybackStart;
+                    buttonPlay.Text = Resources.AUDIO_PLAY;
+
+                    if (player != null)
+                        player.Stop();  // Stop the current audio played
+                }
+
+            }
+            catch (Exception exp)
+            {
+                System.Windows.Forms.MessageBox.Show(Properties.Resources.AUDIOPLAYER_CRASHED_TEXT, Properties.Resources.AUDIOPLAYER_CRASHED_CAPTION,
+                            System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+
+                System.Diagnostics.Trace.WriteLine("AudioDialog Player crashed: " + exp.ToString());
+            }
+        }
+        private delegate void SetControlPropertyThreadSafeDelegate(
+    Control control,
+    string propertyName,
+    object propertyValue);
+
+        public static void SetControlPropertyThreadSafe(
+            Control control,
+            string propertyName,
+            object propertyValue)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new SetControlPropertyThreadSafeDelegate
+                (SetControlPropertyThreadSafe),
+                new object[] { control, propertyName, propertyValue });
+            }
+            else
+            {
+                control.GetType().InvokeMember(
+                    propertyName,
+                    BindingFlags.SetProperty,
+                    null,
+                    control,
+                    new object[] { propertyValue });
+            }
+        }
+        private void PlayerOnOnEnd(EventArgs eventArgs)
+        {
+            SetControlPropertyThreadSafe(buttonRecord, "Enabled", SoundDevicesAvailable.SoundInDeviceAvailable());
+            SetControlPropertyThreadSafe(buttonRemove, "Enabled", true);
+            SetControlPropertyThreadSafe(buttonPlay, "Image", Resources.mediaPlaybackStart);
+            SetControlPropertyThreadSafe(buttonPlay, "Text", Resources.AUDIO_PLAY);
+        }
+
+        private void buttonRecord_Click(object sender, EventArgs e)
+        {
+            if (!recorder.Recording)
+            {
+                RecordPath = System.IO.Path.GetTempFileName();
+                RecordPath = System.IO.Path.ChangeExtension(RecordPath, ".mp3");
+
+                //buttonBrowse.Enabled = false;
+                buttonPlay.Enabled = false;
+                buttonRemove.Enabled = false;
+
+                buttonRecord.Image = Resources.mediaPlaybackStop;
+                buttonRecord.Text = Resources.AUDIO_STOP;
+                recorder.StartRecording(RecordPath, -1, Settings.Default.audioPregap, Settings.Default.audioEndgap);
+            }
+            else
+            {
+                recorder.StopRecording();
+
+                //buttonBrowse.Enabled = true;
+                buttonPlay.Enabled = SoundDevicesAvailable.SoundOutDeviceAvailable();
+                buttonRemove.Enabled = true;
+
+                buttonRecord.Image = Resources.mediaRecord;
+                buttonRecord.Text = Resources.AUDIO_RECORD;
+                if (this.buttonRecordClick != null)
+                {
+                    this.buttonRecordClick(sender, e);
+                }
+            }
+
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            RecordPath = string.Empty;
+            if (this.buttonDeleteClick != null)
+            {
+                this.buttonDeleteClick(sender, e);
+            }
+        }
+    }
+}
